9f76ad8f70dd70996448385ca2d5584c8383dfa2
jesterb0206 <jesterb@seattleu.edu>
2022-11-10 13:50:51 -0800

    Product Database Model

--- models/Product.js
+++ models/Product.js
@@ -1,15 +1,45 @@
-// import important parts of sequelize library
 const { Model, DataTypes } = require("sequelize");
-// import our database connection from config.js
 const sequelize = require("../config/connection");
 
-// Initialize Product model (table) by extending off Sequelize's Model class
+// Initialize Product model (table) by extending off Sequelize's Model class //
+
 class Product extends Model {}
 
-// set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id",
+        unique: false,
+      },
+    },
   },
   {
     sequelize,
