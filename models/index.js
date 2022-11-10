0728e1de686ca58289f459e3798bf80f4535e5d6
jesterb0206 <jesterb@seattleu.edu>
2022-11-10 15:24:55 -0800

    Executed association methods on my Sequelize models

--- models/index.js
+++ models/index.js
@@ -1,16 +1,38 @@
-// import models
+// Import models //
+
 const Product = require("./Product");
 const Category = require("./Category");
 const Tag = require("./Tag");
 const ProductTag = require("./ProductTag");
 
-// Products belongsTo Category
+// Products belongsTo Category //
 
-// Categories have many Products
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 
-// Products belongToMany Tags (through ProductTag)
+// Categories have many Products //
 
-// Tags belongToMany Products (through ProductTag)
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  onDelete: "SET NULL",
+});
+
+// Products belongToMany Tags (through ProductTag) //
+
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+  },
+});
+
+// Tags belongToMany Products (through ProductTag) //
+
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+  },
+});
 
 module.exports = {
   Product,
